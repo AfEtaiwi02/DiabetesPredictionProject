43468d97813ebb18b7c89625cf4396aa7cdc281b
AfEtaiwi02 <154633035+AfEtaiwi02@users.noreply.github.com>
2025-01-29 16:23:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title('Diabetes Prediction Machine Learning project 🩸')
-
+st.write("\n\n\n\n")
 st.info('Hey There! This app takes a set of features and predicts whether the patient has diabetes or not')
